ff6a57fb540ea110211fbd438f0bfea7627651d0
hiltonoliveira <hiltonoliveira@inforgeneses.com.br>
2022-01-23 16:00:25 -0300

    parser comentado

--- Parser.py
+++ Parser.py
@@ -1,18 +1,19 @@
 class Parser:
     def __init__(self, file):
-        self.file = file
-        self.tokens = self.carregar_tokens()
+        self.file = file #salva o caminho do arquivo 
+        self.tokens = self.carregar_tokens() #salva os tokens que estão contidos no arquivo 
     
     def busca_comando(self):
-        return self.tokens.pop(0)
+        return self.tokens.pop(0) #retorna o primeiro comando da lista de comandos e exclui ele da lista
 
     def ha_comandos(self):
-        return self.tokens != []
+        return len(self.tokens) > 0 #retorna se ainda há comandos na lista
 
     def carregar_tokens(self):
-        with open(self.file) as f:
-            lines = map(lambda line: line.split(), f.readlines())
-            tokens = list(filter(lambda x: x[0][0:2] != "//", lines))
-            return tokens
+        with open(self.file) as f: #abre o arquivo
+            lines = map(lambda line: line.split(), f.readlines()) #lê todas as linhas do arquivo
+            tokens = list(filter(lambda x: x[0][0:2] != "//", lines))   # salva todas as linhas que não são comentários (inicio em //)
+            return tokens                                               # em uma lista e retorna
+            
 
     
