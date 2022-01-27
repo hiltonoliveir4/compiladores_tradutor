28f909ba60b07226191baa63b7aa840a8936de3d
hiltonoliveira <hiltonoliveira@inforgeneses.com.br>
2022-01-26 21:11:16 -0300

    atualizando forma de abrir arquivos

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from codeWriter import CodeWriter
 from Parser import Parser
 
-ARITHMETICS = ["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]
+funcoes_aritmeticas = [
+        "add", 
+        "sub", 
+        "neg", 
+        "eq", 
+        "gt", 
+        "lt", 
+        "and", 
+        "or", 
+        "not"
+    ]
 
 def translate(path, code: CodeWriter):
     p = Parser(path)
-
     while p.ha_comandos():
         command = p.busca_comando()
         if command[0] == "push":
             code.writePush(command[1], command[2])
-        elif command[0] in ARITHMETICS:
+        elif command[0] in funcoes_aritmeticas:
             code.writeArithmetic(command)
         elif command[0] == "pop":
             code.writePop(command[1], command[2])
@@ -31,7 +40,27 @@ def translate(path, code: CodeWriter):
 
 
 def main():
-    path = "StackTest.vm"
+    import sys
+    from os import path, listdir
+
+    try:
+        if path.isdir(sys.argv[1]):
+            files = listdir(sys.argv[1])
+            realPath = path.realpath(sys.argv[1])
+            for file in files:
+                if file.endswith('.jack'):
+                    fileName = realPath+"/"+file
+                    print ("compiling",fileName)
+                    path = fileName
+        elif path.isfile(sys.argv[1]):
+            path = sys.argv[1]
+            print (sys.argv[1])
+        else:
+            raise FileNotFoundError
+        
+    except FileNotFoundError:
+        print("File not found.")
+    
     code = CodeWriter(path)
     translate(path, code)
 
